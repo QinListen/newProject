1604a4cf0cb26b72f534735c49452132601e1fe6
QinListen <1715454702@qq.com>
2021-07-26 19:24:59 +0800

    完成了登录功能

---
+++ src/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出功能
+    logout() {
+      // 先清空token
+      window.sessionStorage.clear();
+      // 在利用路由跳转到登录页
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
