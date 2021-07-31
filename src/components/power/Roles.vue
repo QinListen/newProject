b49360fe4c9c79edb9735bba9d74e27acf0b4756
QinListen <1715454702@qq.com>
2021-07-31 14:26:21 +0800

    完成了权限功能的开发

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,283 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card>
+      <!-- 添加角色 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="addJsForm = true"
+            >添加角色</el-button
+          >
+        </el-col>
+      </el-row>
+    </el-card>
+
+    <el-table :data="roleList" border stripe>
+      <!-- 展开列 -->
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <el-row
+            v-for="(item1, i1) in scope.row.children"
+            :key="item1.id"
+            :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']"
+          >
+            <!-- 一级权限 -->
+            <el-col :span="5">
+              <el-tag closable @close="removeRightById(scope.row, item1.id)"
+                >{{ item1.authName }}
+              </el-tag>
+              <i class="el-icon-caret-right"></i>
+            </el-col>
+            <!-- 二级权限/三级权限 -->
+            <el-col :span="19">
+              <!-- 通过for渲染二级权限 -->
+              <el-row
+                :class="[i2 !== 0 ? 'bdtop' : '', 'vcenter']"
+                v-for="(item2, i2) in item1.children"
+                :key="item2.id"
+              >
+                <el-col :span="6">
+                  <el-tag
+                    type="success"
+                    closable
+                    @close="removeRightById(scope.row, item2.id)"
+                    >{{ item2.authName }}</el-tag
+                  >
+                  <i class="el-icon-caret-right"></i>
+                </el-col>
+                <el-col :span="18">
+                  <el-tag
+                    type="warning"
+                    v-for="item3 in item2.children"
+                    :key="item3.id"
+                    closable
+                    @close="removeRightById(scope.row, item3.id)"
+                  >
+                    {{ item3.authName }}
+                  </el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+
+          <pre></pre>
+        </template>
+      </el-table-column>
+      <el-table-column type="index"></el-table-column>
+      <el-table-column label="角色名称" prop="roleName"></el-table-column>
+      <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+      <el-table-column label="操作" width="300px">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini"
+            >编辑</el-button
+          >
+          <el-button type="danger" icon="el-icon-delete" size="mini"
+            >删除</el-button
+          >
+          <el-button
+            type="warning"
+            icon="el-icon-setting"
+            size="mini"
+            @click="showSetRightDialog(scope.row)"
+            >分配权限</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 添加角色 -->
+    <el-dialog
+      title="添加角色"
+      :visible.sync="addJsForm"
+      width="30%"
+      ref="addjsForms"
+    >
+      <!-- 内容主体 -->
+      <el-form :model="jsList" ref="addjss" label-width="70px">
+        <el-form-item label="角色ID" prop="roleId">
+          <el-input v-model="jsList.roleId"></el-input>
+        </el-form-item>
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="jsList.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input v-model="jsList.roleName"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部按钮 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addJsForm = false">取 消</el-button>
+        <el-button type="primary" @click="addjs">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 分配权限对话框 -->
+    <el-dialog
+      title="分配权限"
+      :visible.sync="setRightDialogVisible"
+      width="50%"
+      @close="setRightDialogClosed"
+    >
+      <!-- 树形控件 -->
+      <el-tree
+        :data="rightsList"
+        :props="treeProps"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+        :default-checked-keys="defKeys"
+        ref="treeRef"
+      >
+      </el-tree>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRightDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="allotRights">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      //所有角色列表数据
+      roleList: [],
+      addJsForm: false,
+      jsList: {
+        roleId: "",
+        roleName: "",
+        roleDesc: "",
+      },
+      setRightDialogVisible: false,
+      rightsList: [],
+      // 树形控件
+      treeProps: {
+        label: "authName",
+        children: "children",
+      },
+      // 默认选中的id值数组
+      defKeys: [],
+      // 即将分配角色的id
+      roleId: "",
+    };
+  },
+  created() {
+    this.getRolesList();
+  },
+  methods: {
+    async getRolesList() {
+      const { data: res } = await this.$http.get("roles");
+
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取用户失败!");
+      }
+      this.roleList = res.data;
+      this.$message.success("获取用户成功!");
+    },
+    addjs() {
+      this.$refs.addjsForms.validate(async (valid) => {
+        if (!valid) return;
+        //发起添加用户的网络请求
+        const { data: res } = await this.$http.post("roles", this.addjs);
+
+        if (res.meta.status !== 201) {
+          this.$message.error("添加用户失败");
+        }
+        this.$message.success("添加用户成功");
+        // 点击完成之后隐藏用户对话框
+        this.addJsForm = false;
+        // 由于添加了数据这里要重新调用一次
+        this.getRolesList();
+      });
+    },
+    // 根据id删除权限
+    async removeRightById(role, rightId) {
+      const confirmResult = await this.$confirm(
+        "此操作将永久删除该文件, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).catch((err) => err);
+
+      if (confirmResult !== "confirm") {
+        return this.$message.info("取消了删除");
+      }
+
+      const { data: res } = await this.$http.delete(
+        `roles/${role.id}/rights/${rightId}`
+      );
+
+      if (res.meta.status !== 200) {
+        return this.$message.error("删除权限失败");
+      }
+      // this.getRolesList();
+      role.children = res.data;
+    },
+    //展示分配权限
+    async showSetRightDialog(role) {
+      this.roleId = role.id;
+      // 获取所有权限数据
+      const { data: res } = await this.$http.get("rights/tree");
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取权限数据失败");
+      }
+      this.rightsList = res.data;
+      this.getLeafKeys(role, this.defKeys);
+      this.setRightDialogVisible = true;
+    },
+    // 通过递归，获取三级权限的id，比并保存到defKeys
+    getLeafKeys(node, arr) {
+      if (!node.children) {
+        return arr.push(node.id);
+      }
+      node.children.forEach((item) => {
+        this.getLeafKeys(item, arr);
+      });
+    },
+    // 监听分配权限对话框的关闭事件
+    setRightDialogClosed() {
+      this.defKeys = [];
+    },
+    // 点击为角色分配权限
+    async allotRights() {
+      const keys = [
+        ...this.$refs.treeRef.getCheckedKeys(),
+        ...this.$refs.treeRef.getHalfCheckedKeys(),
+      ];
+
+      const idStr = keys.join(",");
+
+      const { data: res } = await this.$http.post(
+        `roles/${this.roleId}/rights`,
+        { rids: idStr }
+      );
+      if (res.meta.status !== 200) {
+        return this.$message.error("分配角色失败！");
+      }
+      this.$message.success("分配权限成功！");
+      this.getRolesList();
+      this.setRightDialogVisible = false;
+    },
+  },
+};
+</script>
+<style  scoped>
+.el-tag {
+  margin: 7px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+</style>
