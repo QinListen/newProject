fd8689f617c9c67003579532d633300c523ca6d8
QinListen <1715454702@qq.com>
2021-08-13 09:42:27 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -7,10 +7,14 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow thesme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 axios.interceptors.request.use(config => {
-
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 在最后必须 return config 
   return config
@@ -18,6 +22,25 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor, /* { default global options } */)
+
+// 定义一个格式化时间过滤器
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+
+
 new Vue({
   router,
   render: h => h(App)
